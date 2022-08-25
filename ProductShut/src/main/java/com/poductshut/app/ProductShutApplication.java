9c3a9002e36f23a8cb17572e39940ad0fc4107c4
divyanshaTiwari <107752107+divyanshaTiwari@users.noreply.github.com>
2022-08-25 22:24:32 +0530

    Add files via upload

---
+++ ProductShut/src/main/java/com/poductshut/app/ProductShutApplication.java
@@ -0,0 +1,13 @@
+package com.poductshut.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductShutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductShutApplication.class, args);
+	}
+
+}
---
+++ ProductShut/src/test/java/com/poductshut/app/ProductShutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poductshut.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
